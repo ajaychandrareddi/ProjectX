abc174f98e1fa07e5060a3fd78494f425fe813c1
aindukuru <aindukuru@192.168.1.102>
2015-08-16 22:23:05 -0500

    Home to Git

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,16 +1,16 @@
 <?php
 $conn_error = 'Could not Connect. ';
 
-$mysql_host = "";
-$mysql_user = "";
-$mysql_password = "";
+$mysql_host = "localhost";
+$mysql_user = "admin";
+$mysql_password = "admin";
 
-$mysqlconnect = @mysqli_connect($mysql_host, $mysql_user, $mysql_password);
-$mysqldb = "";
+$mysql_connect = @mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysqldb);
+$mysqldb = 'projectx';
 
-if (!@mysqli_connect($mysql_host, $mysql_user, $mysql_password) || !@mysql_select_db($mysqlconnect, $mysqldb))
+if (!@mysqli_connect($mysql_host, $mysql_user, $mysql_password) || !@mysqli_select_db($mysql_connect, $mysqldb))
 {
-	die($conn_error. mysqli_connect_error());
+	die($conn_error);
 	
 }
 
--- includes/form_functions.php
+++ includes/form_functions.php
@@ -1,7 +1,7 @@
 <?php
 
 //Code for searchtypes drop-down
-function mlSearchType(){
+function mlSearchBar(){
 	global $mysql_connect;
 	$result = mysqli_query($mysql_connect, "SELECT * FROM SEARCHTYPES") or die(mysql_error());
 	echo "<select name='searchtype'>";
@@ -9,24 +9,35 @@ function mlSearchType(){
 		echo "<option value='". $row['SEARCHTYPE_ID'] ."'>" .$row['SEARCHTYPE'] ."</option>";
 	}
 	echo "</select>";
-
+	
 	$result2 = mysqli_query($mysql_connect, "SELECT * FROM STATES") or die(mysql_error());
-	echo "<select name='state'>";
-	echo '<option value=0>Select State</option>';
+	echo "<select name='state' onchange=\"reload(this.form)\">";
+		if (empty($_GET['state'])){
+			echo '<option value=0 >Select State</option>';
+		} else {
+			$result2_1 = mysqli_query($mysql_connect, "SELECT STATE FROM STATES WHERE STATE_ID= " .$_GET['state']) or die(mysql_error());
+			echo "<option value=" .$_GET['state'] .">";
+			while ($row = mysqli_fetch_array($result2_1, MYSQLI_ASSOC)){
+				echo $row['STATE'];
+			}
+			echo "</option>";
+		}
 	while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
 		echo "<option value='". $row['STATE_ID'] ."'>" .$row['STATE'] ."</option>";
 	}
 	echo "</select>";
 
-	$result3 = mysqli_query($mysql_connect, "SELECT * FROM CITIES WHERE STATE= " .$row['STATE_ID']) or die(mysql_error());
-	echo "<select name='city'>";
-	echo '<option value=0>Select City</option>';
-	while($row = mysqli_fetch_array($result3, MYSQLI_ASSOC)) {
-		echo "<option value='". $row['CITY_ID'] ."'>" .$row['CITY'] ."</option>";
-	}
+	if (empty($_GET['state'])){
+		echo "<select name='city' disable>";
+		echo '<option value=0>Select City</option></select>';
+	} else {
+		$result3 = mysqli_query($mysql_connect, "SELECT * FROM CITIES WHERE STATE= " .$_GET['state']) or die(mysql_error());
+		echo "<select name='city'>";
+		echo '<option value=0>Select City</option>';
+		while($row = mysqli_fetch_array($result3, MYSQLI_ASSOC)) {
+			echo "<option value='". $row['CITY_ID'] ."'>" .$row['CITY'] ."</option>";
+		}
 	echo "</select>";
+	}
 }
-
-
-
 ?>
